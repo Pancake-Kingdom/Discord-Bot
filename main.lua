0c23874092d5a85de170ab12bec591483425ae67
ingressboy <81222444+ingressboy@users.noreply.github.com>
2021-12-01 19:10:14 +0100

    Grund + Musikbot

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
---- Discord Bot code by ingressboy ---
+local discordia = require('discordia')
+local client = discordia.Client()
+
+client:on('ready', function()
+    local channel = client:getChannel('--')
+    local connection = channel:join()
+    connection:playFFmpeg('music.mp3')
+    print('Logged in as '.. client.user.username)
+end)
+
+client:run('Bot Token')
